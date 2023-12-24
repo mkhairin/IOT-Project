227195fc7c978c945c035ee8cab4d7b0d0a264f8
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2023-12-24 21:14:08 +0800

    Add files via upload

---
+++ sketch_oct6a.ino
@@ -0,0 +1,67 @@
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ThingSpeak.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+String apikey = "HQCULFH0FE0BMFJ7";
+const char *ssid = "OPPO A5S";
+const char *pass = "12345678";
+const char *server = "api.thingspeak.com";
+WiFiClient client;
+unsigned long myChannelNumber = 2271678;
+const char *myWriteAPIKey = "HQCULFH0FE0BMFJ7";
+
+#define DHTPIN D2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Ganti alamat sesuai dengan LCD I2C Anda
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  lcd.begin();  // Perubahan ini
+  Wire.begin(D1,D4);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Suhu: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Kelembapan: ");
+  Serial.println("Connected");
+  WiFi.begin(ssid, pass);
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Sensor Tidak Terdeteksi");
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor Tidak Terdeteksi");
+    lcd.setCursor(0, 1);
+    lcd.print("                "); // Menghapus data sebelumnya
+    delay(2000);
+    return;
+  } else {
+    Serial.print("Suhu: ");
+    Serial.print(temperature);
+    Serial.print(" °C | Kelembapan: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+
+    lcd.setCursor(0, 0);
+    lcd.print(temperature, 1); // Menampilkan suhu dengan satu angka di belakang koma
+    lcd.print(" C");
+
+    lcd.setCursor(0, 1);
+    lcd.print(humidity, 1); // Menampilkan kelembapan dengan satu angka di belakang koma
+    lcd.print(" %");
+    delay(2000);
+  }
+  ThingSpeak.writeField(myChannelNumber, 1, temperature, myWriteAPIKey);
+  ThingSpeak.writeField(myChannelNumber, 2, humidity, myWriteAPIKey);
+}
